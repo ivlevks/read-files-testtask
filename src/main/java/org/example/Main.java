2acff4d51856b712ecf071cc03427b09e29b1751
ivlevks <ivlevks@yandex.ru>
2023-01-17 22:45:22 +0300

    refactor and NullPointer handling with open incorrect fileName

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
         Utils utils = new Utils();
         utils.setParameters(args);
@@ -12,73 +11,10 @@ public class Main {
         List<List<Object>> listDataAllFiles = new ArrayList<>();
         utils.readFiles(listDataAllFiles);
 
-        MergeAlgorithm mergeAlgorithm = new MergeAlgorithm();
+        MergeAlgorithm<Object> mergeAlgorithm = new MergeAlgorithm<>();
         mergeAlgorithm.runMerge(listDataAllFiles);
 
         List<Object> result = mergeAlgorithm.getResult();
         utils.writeData(result);
     }
-
 }
-
-
-//        System.out.println("Error Value");
-//        mergeAlgorithm.getErrorValueInteger().stream().forEach(System.out::println);
-//        mergeAlgorithm.getErrorValueString().stream().forEach(System.out::println);
-//        System.out.println();
-//        System.out.println();
-
-//        System.out.println("Result Value");
-//        for (int i = 0; i < mergeAlgorithm.getResult().size(); i++) {
-//            if (Utils.getOrdering().equals("ASC")) {
-//                System.out.println(mergeAlgorithm.getResult().get(i));
-//            } else System.out.println(mergeAlgorithm.getResult().get(mergeAlgorithm.getResult().size() - 1 - i));
-//        }
-
-
-//        CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
-//        List<Integer> integerList = new ArrayList<>();
-//        try (RandomAccessFile reader = new RandomAccessFile(pathToRead.toFile(), "r")) {
-//
-//            FileChannel fileChannel = reader.getChannel();
-//            MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0L, fileChannel.size());
-//            CharBuffer decodedBuffer = decoder.decode(buffer);
-//
-//            for (int i = 0; i < decodedBuffer.limit(); i++) {
-//                char a = decodedBuffer.get();
-//                integerList.add(Character.getNumericValue(a));
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        integerList.forEach(System.out::println);
-
-
-
-//        System.out.println("Parameters: ");
-//        System.out.println(Utils.getOrdering() + "   " + Utils.getDataType());
-//        System.out.println(outFileName);
-//        inputFileName.forEach(e -> System.out.print(e + "   "));
-//        System.out.println();
-//        System.out.println();
-
-
-
-// обработать если список только 1
-//        list.add(Arrays.asList(1, 2, 3, 0, -1, -6, -123, -56789, -1235435435));
-//        list.add(Arrays.asList(36, 35, 34, 2123));
-//        list.add(Arrays.asList(145, 167, 312, 480, 512));
-//        list.add(Arrays.asList(20, 20, 30, 40, 45));
-//        list.add(Arrays.asList(1200, 2900, -2950, -4280, 15000));
-//        list.add(Arrays.asList(120, 2900, -3584, 4283, 150001));
-//        list.add(Arrays.asList(14, -121, 3680, 4590, 15000000));
-
-
-//        list.add(Arrays.asList("a", "fi", "ml", "nm", "zb"));
-//        list.add(Arrays.asList("b", "bg", "c", "deu", "ijt", "yutfd"));
-//        list.add(Arrays.asList("s", "asd", "jkl", "yi yu"));
-////        list.add(Arrays.asList("qwe", "iuyu", "qweqw", "iuough", "cvxvfghj"));
-////        list.add(Arrays.asList("10", "123", "dqwe", "qwd 12v", "dasdvwer"));
--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -2,10 +2,7 @@ package org.example;
 
 import java.io.*;
 import java.math.BigInteger;
-import java.nio.charset.CharsetDecoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +10,6 @@ public class Utils {
     private static String ORDERING = "ASC";
     private static String DATA_TYPE;
     private final int size = 1024 * 1024;
-    private final CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
     private final List<String> inputFileName = new ArrayList<>();
     private String outFileName = null;
     private static final List<BigInteger> bigIntegers = new ArrayList<>();
@@ -45,38 +41,45 @@ public class Utils {
 
 
     public void readFiles(List<List<Object>> list) {
-        ClassLoader loader = Main.class.getClassLoader();
 
         for (String in : inputFileName) {
             List<Object> midList = new ArrayList<>();
-
-            try (InputStream inputStream = this.getClass()
+            InputStream inputStream = this.getClass()
                     .getClassLoader().getResourceAsStream(in);
-                BufferedReader br = new BufferedReader(
+
+            if (inputStream != null) {
+                try (BufferedReader br = new BufferedReader(
                         new InputStreamReader(inputStream, StandardCharsets.UTF_8), size)) {
 
-                long LINES_TO_READ = 10_000_000;
-                br.lines().limit(LINES_TO_READ).forEach(s -> {
-                    try {
-                        if (s.length() != 0) {
-                            if (DATA_TYPE.equals("INTEGER")) {
-                                try {
-                                    int num = Integer.parseInt(s);
-                                    midList.add(num);
-                                } catch (NumberFormatException m) {
-                                    bigIntegers.add(new BigInteger(s));
+                    long LINES_TO_READ = 10_000_000;
+                    br.lines().limit(LINES_TO_READ).forEach(s -> {
+                        try {
+                            if (s.length() != 0) {
+                                if (DATA_TYPE.equals("INTEGER")) {
+                                    try {
+                                        int num = Integer.parseInt(s);
+                                        midList.add(num);
+                                    } catch (NumberFormatException m) {
+                                        bigIntegers.add(new BigInteger(s));
+                                    }
+                                } else {
+                                    midList.add(s);
                                 }
-                            } else {
-                                midList.add(s);
                             }
+                        } catch (NumberFormatException e) {
+                            System.out.println(e.getMessage() + ": " + s +  " invalid value for " + DATA_TYPE +
+                                    ". No possible compare. This value will lost.");
                         }
-                    } catch (NumberFormatException e) {
-                        System.out.println(e.getMessage() + ": " + s +  " invalid value for " + DATA_TYPE +
-                                ". No possible compare. This value will lost.");
+                    });
+                } catch (IOException m) {
+                    System.out.println(m.getMessage());
+                } finally {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
                     }
-                });
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+                }
             }
             list.add(midList);
         }
@@ -99,9 +102,6 @@ public class Utils {
         }
     }
 
-    public static String getOrdering() {
-        return ORDERING;
-    }
 
     public static String getDataType() {
         return DATA_TYPE;
